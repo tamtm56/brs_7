f16b5068b2624ab312b5285e39be1ed67b00212d
hieu <hieu@ubuntu.(none)>
2015-03-27 15:16:34 +0700

    show info book

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  
-
-  #get 'books/index'
-
   root "static_pages#home" 
+
   get "help"    => "static_pages#help"
   get "about"   => "static_pages#about"
   get "contact" => "static_pages#contact"
